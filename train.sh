9e894ab13f4861d6eeb37c10d10f32a7d20fc143
hhw <huhanwenxxx@163.com>
2018-11-30 10:10:20 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,29 @@
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+export CUDA_VISIBLE_DEVICES=0
+
+CURRENT_DIR=$(pwd)
+
+TFRECORD_DIR="$CURRENT_DIR/tfrecords/quadrilateral_2"
+DATASET_SPLIT="train"
+MODEL_VARIANT="resnet_v1_50_beta"
+RESTORE_CKPT_PATH="$CURRENT_DIR/init_models/resnet_v1_50/model.ckpt"
+TRAIN_DIR="$CURRENT_DIR/exp/quadrilateral_2_resnet_v1_50_beta_01/train"
+NUM_EPOCHS=400
+DECAY_EPOCHS=200
+
+python "$CURRENT_DIR"/regression_train.py \
+  --tfrecord_dir=$TFRECORD_DIR \
+  --dataset_split=$DATASET_SPLIT \
+  --model_variant=$MODEL_VARIANT \
+  --restore_ckpt_path=$RESTORE_CKPT_PATH \
+  --train_dir=$TRAIN_DIR \
+  --batch_size=64 \
+  --is_training=True \
+  --initial_learning_rate=0.0001 \
+  --decay_epochs=$DECAY_EPOCHS \
+  --staircase=True \
+  --num_epochs=$NUM_EPOCHS \
+  --save_checkpoint_steps=500 \
+  --log_frequency=10
